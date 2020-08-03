902ee644ed6153a121436e1524188cddd6291b3d
mouraleonardo <mouraleonardo@hotmail.com>
2020-08-03 17:07:57 -0400

    WellBeings PR 004 - Final test application

--- WellBeings/Controllers/HomeController.cs
+++ WellBeings/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using WellBeings.Data;
+using Contact.Models;
 
 namespace WellBeings.Controllers
 {
@@ -38,10 +40,47 @@ namespace WellBeings.Controllers
             ViewBag.Retailers = _retailersDataAccess.GetRetailers();
             return View();
         }
+
+        // GET: Contact
+        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Our team will be happy to answer store and product-related questions.";
+            ViewBag.Message = null;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactUs vm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msz = new MailMessage();
+                    msz.From = new MailAddress(vm.Email);
+                    msz.To.Add("yourEmailAdress@gmail.com");
+                    msz.Subject = vm.Subject;
+                    msz.Body = vm.Message;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.Credentials = new System.Net.NetworkCredential
+                        ("yourEmailAdress@gmail.com", "yourActualPassword");
+                    smtp.EnableSsl = true;
+                    smtp.Send(msz);
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for Contacting us.";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = "Sorry your message was not sent." + ex;
+                }
+            }
 
+            return View();
+        }
+        public ActionResult Error()
+        {
             return View();
         }
     }
---
+++ WellBeings/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Contact.Models
+{
+    public class ContactUs
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "Name cannot be longer than 30 characters.")]
+        [Display(Name = "Your Name.")]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Subject cannot be longer than 50 characters.")]
+        [Display(Name = "Your subject.")]
+        public string Subject { get; set; }
+        [Required]
+        [StringLength(1000, ErrorMessage = "Message cannot be longer than 1000 characters.")]
+        [Display(Name = "Your message.")]
+        public string Message { get; set; }
+    }
+}
